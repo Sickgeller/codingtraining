0af9d06634af98c6b4504883436388c8d5edff88
user <user@DESKTOP-S6UUK9H>
2025-02-04 11:58:43 +0900

    123

--- src/codingtraining/baekjoon1865.java
+++ src/codingtraining/baekjoon1865.java
@@ -11,8 +11,7 @@ import java.util.StringTokenizer;
 
 public class baekjoon1865 {
 
-	//벨만포드 알고리즘 예제
-
+	// 벨만포드 알고리즘을 사용하여 음의 사이클을 체크하는 코드
 	static int TC, N, M, W, S, E, T;
 	static ArrayList<Edge> list;
 	static boolean isCircular;
@@ -30,7 +29,7 @@ public class baekjoon1865 {
 			M = Integer.parseInt(st.nextToken());
 			W = Integer.parseInt(st.nextToken());
 
-			// 도로 입력
+			// 도로 입력 (양방향 간선 추가)
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine());
 				S = Integer.parseInt(st.nextToken()); // 시작점
@@ -50,28 +49,29 @@ public class baekjoon1865 {
 			}
 
 			// Bellman-Ford 알고리즘을 위한 초기화
-			int[] dist = new int[N + 1]; // 노드 번호는 1부터 N까지
-			Arrays.fill(dist, Integer.MAX_VALUE);
-			dist[1] = 0; // 시작점(1번)의 거리는 0으로 설정
+			isCircular = false;  // 음수 사이클 체크 변수 초기화
 
 			// 벨만포드 알고리즘 수행
-			for (Edge edge : list) {
-				if(dist[edge.start] == Integer.MAX_VALUE) continue;
-				if(dist[edge.start] + edge.weight < dist[edge.destination]) {
-					dist[edge.destination] = dist[edge.start] + edge.weight;
+			int[] dist = new int[N + 1]; // 노드 번호는 1부터 N까지
+			Arrays.fill(dist, 100000001); // 모든 거리를 무한대로 초기화
+			dist[1] = 0; // 시작점의 거리는 0으로 설정
+
+			// N-1번 반복하여 최단 거리 갱신
+			for (int i = 1; i < N; i++) {
+				for (Edge edge : list) {
+					if (dist[edge.start] + edge.weight < dist[edge.destination]) {
+						dist[edge.destination] = dist[edge.start] + edge.weight;
+					}
 				}
 			}
 
-			// 음수 사이클 체크
-			isCircular = false;
+			// N번째 반복에서 갱신이 되면 음수 사이클이 있다는 뜻
 			for (Edge edge : list) {
-				if(dist[edge.start] == Integer.MAX_VALUE) continue;
-				if(dist[edge.start] + edge.weight < dist[edge.destination]) {
+				if (dist[edge.start] + edge.weight < dist[edge.destination]) {
 					isCircular = true;
 					break;
 				}
 			}
-
 			// 결과 출력
 			if (isCircular) {
 				bw.write("YES\n");
@@ -80,7 +80,10 @@ public class baekjoon1865 {
 			}
 		}
 		bw.flush();
+		bw.close();
+		br.close();
 	}
+
 }
 
 class Edge {
